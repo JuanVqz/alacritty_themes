2ed305a5abbba96aaada9624d29a5d27ed413317
Juan Vásquez <juan@logicalbricks.com>
2022-03-16 21:59:13 -0600

    Add basic config (#1)
    - [x] add rubocop configuration
    
    - [x] Add GitHub action

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+## [Unreleased]
+
+## [0.1.1] - 2022-03-16
+
+- Add rubocop configuration
+- Add github workflow
+
+## [0.1.0] - 2022-03-15
+
+- Initial commit
--- spec/alacritty_themes_spec.rb
+++ spec/alacritty_themes_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe AlacrittyThemes do
   it "has a version number" do
     expect(AlacrittyThemes::VERSION).not_to be nil
